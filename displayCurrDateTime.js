a4d6bd83e1b9087bff4fbb6ee677d2e298037900
Mehar Middha <103279305+MeharMiddha@users.noreply.github.com>
2023-08-19 16:27:45 +0530

    Create displayCurrDateTime.js
    in this we have used javascript to display current date and current time

---
+++ displayCurrDateTime.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const name = "Mehar Middha";
+const currDate = new Date().toLocaleDateString();
+const currTime = new Date().toLocaleTimeString();
+
+ReactDOM.render(
+  <>
+    <h1>My Name is {name}</h1>
+    <p> Current Date is = {currDate} </p>
+    <p> Current Time is = {currTime} </p>
+  </>,
+  document.getElementById("root")
+);

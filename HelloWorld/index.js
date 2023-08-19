14e3869c2c886e25cb34b0046aba626b5c0fa9eb
Mehar Middha <103279305+MeharMiddha@users.noreply.github.com>
2023-08-19 12:48:55 +0530

    Update index.js
    to display hello world in react

--- HelloWorld/index.js
+++ HelloWorld/index.js
@@ -3,4 +3,22 @@ import React from 'react';
 // var ReactDOM = require('react-dom');
 import ReactDOM from 'react-dom';
 
+
+// This is the code we write in react
+
 ReactDOM.render( <h1> Hello World!! </h1> , document.getElementById('root'));
+
+
+
+// the code we wrote in react is changed into javascript by babelscript so that it becomes easy to understand by the compiler
+// ReactDOM.render( React.createElement("h1",
+// null,
+// " Hello World!! "),
+// document.getElementById("root"));
+
+
+
+//it is pure javascript code
+// var h1 = document.createElement("h1");
+// h1.innerHTML = " Hello World!! ";
+// document.getElementById("root").appendChild(h1);

1a3fa35659a770dcbf834912a77d97bd5fc4d285
Mehar Middha <103279305+MeharMiddha@users.noreply.github.com>
2023-08-22 00:29:54 +0530

    Update index.js
    this is the updated js file of our mini project

--- Mini-Project 1/index.js
+++ Mini-Project 1/index.js
@@ -1,31 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
 
-let currTime = new Date().getHours();
-let greeting = '';
-
-const cssStyle= {};
-
-
-if(currTime>0 && currTime<12){
-    greeting = 'Good Morning';
-    cssStyle.color='green';
-}
-else if(currTime>=12 && currTime<=19){
-  greeting = 'Good Afternoon'
-  cssStyle.color='Orange';
-}
-else{
-  greeting = 'Good Night'
-  cssStyle.color='Black';
-}
-
-ReactDOM.render(
-  <>
-    <div>
-      <h1>Hello Sir, <span style={cssStyle}>{greeting}</span></h1>
-    </div>
-  </>,
-  document.getElementById("root")
-);
+ReactDOM.render( <App />,document.getElementById("root") );

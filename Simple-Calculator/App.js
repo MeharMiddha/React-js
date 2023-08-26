ad053423a06c905eb2365ee1b6d500bc8f9e8d7b
Mehar Middha <103279305+MeharMiddha@users.noreply.github.com>
2023-08-26 23:11:26 +0530

    Create App.js

---
+++ Simple-Calculator/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {add,sub,div,mult} from './Calc';
+
+function App(){
+  return(
+    <>
+      <ul>
+        <li> Sum of two no is {add(40,4)} </li>
+        <li> Sub of two no is {sub(30,3)} </li>
+        <li> Div od two no is {div(10,3)} </li>
+        <li> Mult of two no is {mult(10,3)} </li>
+      </ul>
+    </>
+  )
+}
+
+export default App;

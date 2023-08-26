5d47255748c4e27f3dea9ffe2e06ec7adb091842
Mehar Middha <103279305+MeharMiddha@users.noreply.github.com>
2023-08-26 22:43:22 +0530

    Create App.js

---
+++ Import-Export/App.js
@@ -0,0 +1,12 @@
+const youtuber = "Mehar Middha";
+
+const favprog = 'React js';
+var name = "Mehar"
+
+function myName() {
+    return ( name = "Karan" );
+}
+
+export default youtuber;
+
+export {favprog,myName};

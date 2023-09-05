8aad14d5e7d3a648d01f0e922159a71d1a153db7
Mehar Middha <103279305+MeharMiddha@users.noreply.github.com>
2023-09-05 15:08:40 +0530

    Create Sdata.jsx

---
+++ Netflix-Project/Sdata.jsx
@@ -0,0 +1,40 @@
+const Sdata = [
+    {
+        sname:'DARK',
+        imgsrc:"https://i.pinimg.com/originals/cd/67/78/cd67786db8f2bef016b9d4e924ecaf03.jpg",
+        title:" A Netflix Orignal Series ",
+        link:'https://www.netflix.com/search?q=dark&jbv=80100172',
+    },
+    {
+        sname:'The Vampire Diaries',
+        imgsrc:"https://is3-ssl.mzstatic.com/image/thumb/Music69/v4/4e/f9/90/4ef9901a-300a-e140-b74a-740ea7d28671/source/3000x3000sr.jpg",
+        title:" A Netflix Orignal Series ",
+        link:'https://www.netflix.com/search?q=the%20va&jbv=70143860',
+    },
+    {
+        sname:'The Vikings',
+        imgsrc:"https://wallpapers-all.com/uploads/posts/2016-12/20_vikings_tv_show.jpg",
+        title:" A Netflix Orignal Series ",
+        link:'https://www.netflix.com/search?q=the%20vikings&jbv=70301870',
+    },
+    {
+        sname:'Peaky Blinders',
+        imgsrc:"https://browsedesk.com/wp-content/uploads/2020/08/Peaky-Blinders-Season-6-1024x576.jpg",
+        title:" A Netflix Orignal Series ",
+        link:'https://www.netflix.com/search?q=peaky&jbv=80002479',
+    },
+    {
+        sname:'Money Heist',
+        imgsrc:"https://image.tmdb.org/t/p/original/zAIippNnm6o0gYEtjapbjQSxP8G.jpg",
+        title:" A Netflix Orignal Series ",
+        link:'https://www.netflix.com/search?q=money&jbv=80192098',
+    },
+    {
+        sname:'Friends',
+        imgsrc:"https://wallpapercave.com/wp/wp2237255.jpg",
+        title:" A Netflix Orignal Series ",
+        link:'https://www.netflix.com/search?q=friends&jbv=70153404',
+    },
+];
+
+export default Sdata;

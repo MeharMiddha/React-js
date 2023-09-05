7be635de3a07f4639e21fd06827c6292a859c686
Mehar Middha <103279305+MeharMiddha@users.noreply.github.com>
2023-09-05 12:38:48 +0530

    Create index.js

---
+++ Props/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Card from './Cards';
+
+ReactDOM.render(
+  <>
+    <Card imgsrc="https://i.pinimg.com/originals/cd/67/78/cd67786db8f2bef016b9d4e924ecaf03.jpg"
+      title=" A Netflix Orignal Series "
+      sname='DARK'
+      link='https://www.netflix.com/search?q=dark&jbv=80100172'
+    />
+
+    <Card imgsrc="https://is3-ssl.mzstatic.com/image/thumb/Music69/v4/4e/f9/90/4ef9901a-300a-e140-b74a-740ea7d28671/source/3000x3000sr.jpg"
+      title=" A Netflix Orignal Series "
+      sname='The Vampire Diaries'
+      link='https://www.netflix.com/search?q=the%20va&jbv=70143860'
+    />
+    <Card imgsrc="https://wallpapers-all.com/uploads/posts/2016-12/20_vikings_tv_show.jpg"
+      title=" A Netflix Orignal Series "
+      sname='The Vikings'
+      link='https://www.netflix.com/search?q=the%20vikings&jbv=70301870'
+    />
+    <Card imgsrc="https://browsedesk.com/wp-content/uploads/2020/08/Peaky-Blinders-Season-6-1024x576.jpg"
+      title=" A Netflix Orignal Series "
+      sname='Peaky Blinders'
+      link='https://www.netflix.com/search?q=peaky&jbv=80002479'
+    />
+    <Card imgsrc="https://image.tmdb.org/t/p/original/zAIippNnm6o0gYEtjapbjQSxP8G.jpg"
+      title=" A Netflix Orignal Series "
+      sname='Money Heist'
+      link='https://www.netflix.com/search?q=money&jbv=80192098'
+    />
+    <Card imgsrc="https://wallpapercave.com/wp/wp2237255.jpg"
+      title=" A Netflix Orignal Series "
+      sname='Friends'
+      link='https://www.netflix.com/search?q=friends&jbv=70153404'
+    />
+  </>,
+  document.getElementById("root")
+);
